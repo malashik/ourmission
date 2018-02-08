b894ecc61e46ac68e4d84a3903a6e3a521523345
Aleksandr Malash <a.malash@godeltech.com>
2018-02-08 15:56:52 +0300

    Slider without btns, only with controls

--- src/slider/slider.jsx
+++ src/slider/slider.jsx
@@ -15,25 +15,20 @@ class Slider extends Component {
 
   defineInitialState() {
     const Slider = document.querySelector(".Slider");
-    console.log("defineOffset()!!!", Slider.offsetWidth);
     let Offset = Slider.offsetWidth;
     this.setState({
       offset: Offset,
       length: this.getChildrenList().length
     });
   }
+
   defineOffset() {
     const Slider = document.querySelector(".Slider");
-    const Controls = document.querySelector(".Slider__control-points");
-
     let offset = Slider.offsetWidth;
-    console.log("defineOffset()!!!", Slider.offsetWidth);
     return offset;
   }
 
   getChildrenList() {
-    console.log("getChildrenList()");
-
     return this.props.children.map((child, index) => (
       <li
         key={"Slider__item" + index}
@@ -46,8 +41,6 @@ class Slider extends Component {
   }
 
   getControlList() {
-    console.log("getControlList()");
-
     return this.props.children.map((child, index) => (
       <li
         key={"Slider__control-points-item" + index}
@@ -59,18 +52,7 @@ class Slider extends Component {
 
   componentDidMount() {
     this.defineInitialState();
-
     setInterval(this.moveSlide.bind(this), 1000);
-    // clearTimeout(setInterval(this.moveSlide.bind(this), 1000));
-
-    // setInterval(() => {
-    //   const that = this;
-    //   console.log('click');
-    //   if (this.state.setTimeoutSwitch) {
-    //     this.moveSlide.bind(that);
-    //   }
-
-    // }, 1000);
   }
 
   moveSlide() {
@@ -95,9 +77,9 @@ class Slider extends Component {
       }
     }
   }
+
   controlMove = function(e) {
     let val = e.target.id.slice(-1);
-    // console.log(e.target.tagName === 'LI');
     if (e.target.tagName === "LI") {
       this.setState({
         count: +val + 1,
@@ -122,10 +104,6 @@ class Slider extends Component {
             onClick={e => this.controlMove(e)}
           >
             {this.getControlList()}
-          </div>
-          {this.state.count}
-          <div className="Slider__next" onClick={() => this.moveSlide()}>
-            NEXT
           </div>
         </div>
       </div>
